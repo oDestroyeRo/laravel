56ea027d6cd7df387908208c1fb9f5af140ac668
Unknown <natpakan.w@ku.th>
2017-04-18 16:36:22 +0700

    aaaaaaaaaaaaaa

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/about-me', function () {
     return view('about');
 });
 
-Rount::get('posts/{id}/{name}', 
+Rount::get('/posts/{id}/{name?}', 
 	'PostController@show');
 
 Route::get('master', function () {
